fbe320ef7090beed204cc8220705e3fe121d6f82
aryanmishra11 <aryanmishra441@gmail.com>
2024-05-10 14:12:54 +0530

    first commit

---
+++ chatbot/chat_app/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class ChatBot(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="GeminiUser", null=True
+    )
+    text_input = models.CharField(max_length=500)
+    gemini_output = models.TextField(null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+
+    def __str__(self):
+        return self.text_input
---
+++ chatbot/chat_app/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, reverse
+from django.contrib.auth.decorators import login_required
+from .models import ChatBot
+from django.http import HttpResponseRedirect, JsonResponse
+import google.generativeai as genai
+
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR,'.env'))
+import google.generativeai as genai
+genai.configure(api_key=os.getenv("GOOGLE_APIKEY"))
+# Create your views here.
+# add here to your generated API key
+
+
+@login_required
+def ask_question(request):
+    if request.method == "POST":
+        text = request.POST.get("text")
+        model = genai.GenerativeModel("gemini-pro")
+        chat = model.start_chat()
+        response = chat.send_message(text)
+        user = request.user
+        ChatBot.objects.create(text_input=text, gemini_output=response.text, user=user)
+        # Extract necessary data from response
+        response_data = {
+            "text": response.text,  # Assuming response.text contains the relevant response data
+            # Add other relevant data from response if needed
+        }
+        return JsonResponse({"data": response_data})
+    else:
+        return HttpResponseRedirect(
+            reverse("chat")
+        )  # Redirect to chat page for GET requests
+
+
+@login_required
+def chat(request):
+    user = request.user
+    chats = ChatBot.objects.filter(user=user)
+    return render(request, "chat_bot.html", {"chats": chats})
+
